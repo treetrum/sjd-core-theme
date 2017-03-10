e98401c95dd72732ad6c0f6f8ae3e8dde067ea7a
Sam Davis <sam@sjd.co>
2017-03-11 10:38:33 +1100

    Bootstrap core template files

---
+++ functions.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Enqueue scripts & styles
+|--------------------------------------------------------------------------
+*/
+
+function sjd_enqueue_scripts_and_styles() {
+
+	// Use our own version of jQuery
+    wp_deregister_script( 'jquery' );
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/bower_components/jquery/dist/jquery.min.js', null, null, false );
+
+    // User script/s
+    wp_enqueue_script( 'app', get_template_directory_uri() . '/dist/js/app-min.js', null, null, true );
+
+	// Pass WordPress data into our JS
+    $js_data = [
+        'siteURL' => get_site_url(),
+        'themeURL' => get_stylesheet_directory_uri()
+    ];
+    wp_localize_script( 'app', 'siteData',  $js_data);
+
+	// User style/s
+	wp_enqueue_style('main', get_template_directory_uri() . '/dist/css/main.css?v=' . md5_file(get_template_directory_uri() . '/dist/css/main.css'));
+
+}
+add_action( 'wp_enqueue_scripts', 'sjd_enqueue_scripts_and_styles' );
+
+
+/*
+|--------------------------------------------------------------------------
+| Basic setup
+|--------------------------------------------------------------------------
+*/
+
+// Hide admin bar
+show_admin_bar(false);
+
+// Clean up header
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+
+// Theme support
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- FontAwesome -->
+    <script src="https://use.fontawesome.com/92d1da124e.js"></script>
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
