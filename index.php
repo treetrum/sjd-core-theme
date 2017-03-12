5dfde92cdfdda006afc4391ce59624a44c00acc9
Sam Davis <sam@sjd.co>
2017-03-13 10:32:31 +1100

    Give a *bit* more style to index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php the_title(); ?>
+<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
 <?php the_content(); ?>
 
